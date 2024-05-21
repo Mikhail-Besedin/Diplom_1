efc3735805aac880055233923e81749ffd0dfbb8
Mikhail Besedin <besedin-misha2014@yandex.ru>
2024-05-22 01:52:41 +0300

    Исправлен тест test_get_receipt_burger_success. Переделана проверка вывода рецепта. Обновлен отчет о покрытии.

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -6,25 +6,22 @@ from praktikum.burger import Burger
 
 class TestBurger:
 
-    def test_set_buns_in_burger_success(self,create_bun):
+    def test_set_buns_in_burger_success(self, create_bun):
         test_burger = Burger()
         test_burger.set_buns(create_bun)
         assert test_burger.bun == create_bun
 
 
-
     @pytest.mark.parametrize("test_ingredient", [
         "create_ingredient_first",
         "create_ingredient_second"])
-    def test_add_ingredient_in_burger_success(self,test_ingredient):
+    def test_add_ingredient_in_burger_success(self, test_ingredient):
         test_burger = Burger()
         test_burger.add_ingredient(test_ingredient)
         assert test_ingredient in list(test_burger.ingredients)
 
 
-
-
-    def test_remove_ingredient_in_burger_success(self,create_ingredient_first,create_ingredient_second):
+    def test_remove_ingredient_in_burger_success(self, create_ingredient_first, create_ingredient_second):
         test_burger = Burger()
         test_burger.add_ingredient(create_ingredient_first)
         test_burger.add_ingredient(create_ingredient_second)
@@ -32,20 +29,15 @@ class TestBurger:
         assert len(test_burger.ingredients) == 1 and create_ingredient_second not in list(test_burger.ingredients)
 
 
-
-
-    def test_move_ingredient_in_burger_success(self,create_ingredient_first,create_ingredient_second):
+    def test_move_ingredient_in_burger_success(self, create_ingredient_first, create_ingredient_second):
         test_burger = Burger()
         test_burger.add_ingredient(create_ingredient_first)
         test_burger.add_ingredient(create_ingredient_second)
-        test_burger.move_ingredient(1,0)
+        test_burger.move_ingredient(1, 0)
         assert create_ingredient_second == test_burger.ingredients[0]
 
 
-
-
-
-    def test_get_price_burger_success(self,create_bun, create_ingredient_first, create_ingredient_second):
+    def test_get_price_burger_success(self, create_bun, create_ingredient_first, create_ingredient_second):
         test_burger = Burger()
         test_burger.set_buns(create_bun)
         test_burger.add_ingredient(create_ingredient_first)
@@ -53,12 +45,13 @@ class TestBurger:
         assert test_burger.get_price() == DataTest.burger_price
 
 
-
-
-    def test_get_receipt_burger_success(self,create_bun, create_ingredient_first, create_ingredient_second):
+    def test_get_receipt_burger_success(self, create_bun, create_ingredient_first, create_ingredient_second):
         test_burger = Burger()
         test_burger.set_buns(create_bun)
         test_burger.add_ingredient(create_ingredient_first)
         test_burger.add_ingredient(create_ingredient_second)
-        assert DataTest.test_ingredient_name_2 in test_burger.get_receipt()
-
+        receipt = f'''(==== {test_burger.bun.get_name()} ====)
+= {str(create_ingredient_first.get_type())} {create_ingredient_first.get_name()} =
+= {str(create_ingredient_second.get_type())} {create_ingredient_second.get_name()} =
+(==== {test_burger.bun.get_name()} ====)'''
+        assert receipt in test_burger.get_receipt()
